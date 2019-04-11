5d4b56330f6ca8deda0b45535f38586551e5ef24
JD Fillmore <jd@jdfill.me>
2019-04-11 02:12:26 -0400

    remove sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,10 +36,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    siteMetadata: {
-      siteUrl: `https://jdfill.me`,
-    },
-    plugins: [`gatsby-plugin-sitemap`]
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
