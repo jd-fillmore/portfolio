fffee66f74870786fbac2bb6631f2920dbaab0eb
JD Fillmore <jd@dfill.me>
2019-11-08 13:55:06 -0500

    seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import Work from "../components/work/work"
 const Home = () => (
   <Layout>
     <SEO
-      title="Front End Developer Portfolio | React &amp; WordPress | Toronto, ON"
-      description="JD Fillmore's front end developer, React and WordPress portfolio. Click here to start your next project. Based in Toronto, ON."
+      title="Front End Developer | React &amp; WordPress | St. Catharines, Niagara, ON"
+      description="JD Fillmore is a front end developer from St. Catharines, Niagara, ON. Specializing in WordPress and React. Click here to contact!"
     />
     <Hero
       title="Hi! I'm JD. A frontend developer &amp; UI designer that creates interactive online experiences."
