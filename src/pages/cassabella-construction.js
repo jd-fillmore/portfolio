ed10c471051fe456dcc6b49102a04f70c12b9648
JD Fillmore <jd@jdfill.me>
2020-03-14 00:19:58 -0400

    update cass link

--- src/pages/cassabella-construction.js
+++ src/pages/cassabella-construction.js
@@ -23,7 +23,7 @@ const FF7 = () => (
           introImage={cassIntroImage}
           altTag="cass intro image"
           projectTitle="Cass-A-Bella Construction"
-          projectLink="https://cassabella.netlify.com"
+          projectLink="https://cassabellaconstruction.com"
           projectDescription="Reached out to Cass-A-Bella Construction, offering to re-design and re-build their website on a modern platform. Their current website was very outdated in terms of design and technology stack. I decided to completely re-design and re-build their website from the ground up, using modern design and front-end development technologies. The website is now faster, more performant and accessible, and looks a lot nicer than previously."
           techUsed="HTML5, CSS3, Sass, JavaScript, Gatsby, React, NPM, Flexbox, Sketch"
           altSpeedScore="cassabella score"
