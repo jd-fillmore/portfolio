d8674e24e8c39deb8afe2839094c832663f89010
JD Fillmore <jd@jdfill.me>
2020-10-13 09:33:09 -0400

    adjust hero mobile link text

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const Home = () => (
     />
     <Hero
       title="Front-end developer & UI designer."
-      description="Seasoned remote front-end developer with a core focus on driving business results. Experienced in developing websites and web applications that deliver on impact, functionality and accessibility. Employed at one of Canada’s largest equipment rental operations."
+      description="Seasoned remote front-end developer &amp; UI designer with a core focus on driving business results. Experienced in developing websites and web applications that deliver on impact, functionality and accessibility. Employed at one of Canada’s largest equipment rental operations."
     />
     <Skills title="Recent Technology Stack" />
     <Work />
